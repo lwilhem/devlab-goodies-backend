e8485700a429dced3698758a4e6e120846cb6e6a
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-02 16:02:36 +0200

    :package: finished basic API Setup (Docs & Global Prefix)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Devlab API')
+    .setDescription('WIP Description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
