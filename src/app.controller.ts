ca456ff351fc2b1afa1c711a0309ec4b9197a26d
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-02 18:00:03 +0200

    :goal_net: Improved HTTP Error Handling

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Controller()
+@UseFilters(HttpExceptionFilter)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
