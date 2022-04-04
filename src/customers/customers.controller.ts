6acc293a02ffd41fc5cd5768926141d2d67e3aa2
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-04 18:29:50 +0200

    :construction: Core CRUD Business Logic

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, UseFilters } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { HttpExceptionFilter } from '../filters/http-exception.filter';
 import { CustomersService } from './customers.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CustomerEntity } from './entities/customer.entity';
 
 @Controller('customers')
 @UseFilters(HttpExceptionFilter)
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
+
+  @Post('create')
+  async createOneCustomer(
+    @Body() data: CreateCustomerDto,
+  ): Promise<CustomerEntity> {
+    const newCustomer = await this.customersService.createCustomer(data);
+    return newCustomer;
+  }
+
+  @Get('search')
+  async readAllCustomers() {
+    const customers = await this.customersService.findAllCustomers();
+    return customers;
+  }
 }
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { CustomerEntity } from './entities/customer.entity';
 
 @Injectable()
@@ -9,4 +11,26 @@ export class CustomersService {
     @InjectRepository(CustomerEntity)
     private customerEntity: Repository<CustomerEntity>,
   ) {}
+
+  async createCustomer(data: CreateCustomerDto): Promise<CustomerEntity> {
+    const newUser = this.customerEntity.save(data);
+    return newUser;
+  }
+
+  async findAllCustomers() {
+    return await this.customerEntity.find();
+  }
+
+  async updateCustomer(target_id: number, data: UpdateCustomerDto) {
+    const updatedData = await this.customerEntity.update(
+      { id: target_id },
+      data,
+    );
+    return updatedData;
+  }
+
+  async deleteCustomer(id: number) {
+    const deleteUser = await this.customerEntity.delete({ id: id });
+    return deleteUser;
+  }
 }
