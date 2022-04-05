5597c0bb4306bd4823638d661a168fb29fc3700f
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-05 15:06:48 +0200

    :construction: Auth Module Architecture

---
+++ src/auth/controller/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, UseFilters } from '@nestjs/common';
+import { HttpExceptionFilter } from '../../filters/http-exception.filter';
+import { AuthService } from '../service/auth.service';
+
+@Controller('auth')
+@UseFilters(HttpExceptionFilter)
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async registerCustomer() {
+    return 'Registered Customer';
+  }
+}
--- src/authentication/authentication.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationController } from './authentication.controller';
-import { AuthenticationService } from './authentication.service';
-
-@Module({
-  providers: [AuthenticationService],
-  controllers: [AuthenticationController],
-})
-export class AuthenticationModule {}
