384dc45e3a7b288198e0f1963154789bb2bfd01e
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-06 17:50:13 +0200

    customer Architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true, //TURN TO FALSE IN PRODUCTION
     }),
+    CustomersModule,
   ],
   controllers: [],
   providers: [],
