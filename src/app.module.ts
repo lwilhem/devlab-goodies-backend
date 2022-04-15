b76793c729d758c9a59ad65cd5907ec7feb1c7aa
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-15 15:16:50 +0200

    Cart Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { ShopsModule } from './shops/shops.module';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ShopsModule } from './shops/shops.module';
     DatabaseModule,
     ProductsModule,
     ShopsModule,
+    CartsModule,
   ],
   controllers: [],
   providers: [],
